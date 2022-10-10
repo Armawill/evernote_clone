49aa060290490db472acb3b7f12f24dbb63c4743
Armawill <armawill@gmail.com>
2022-10-10 12:56:14 +0300

    Added notebook name validation
    Added name validation when creating a notebook

--- lib/screens/add_notebook_screen.dart
+++ lib/screens/add_notebook_screen.dart
@@ -1,23 +1,40 @@
+import 'package:evernote_clone/presentation/custom_icons_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../notebook/notebook.dart';
 
-class AddNotebookScreen extends StatelessWidget {
+class AddNotebookScreen extends StatefulWidget {
   static const routeName = '/add-notebook';
+
+  @override
+  State<AddNotebookScreen> createState() => _AddNotebookScreenState();
+}
+
+class _AddNotebookScreenState extends State<AddNotebookScreen> {
   final _controller = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+
+  String title = '';
+
+  bool _isExist = false;
+
   @override
   Widget build(BuildContext context) {
+    var notebookBloc = BlocProvider.of<NotebooksBloc>(context);
     return Scaffold(
       appBar: AppBar(
         actions: [
           TextButton(
-            onPressed: () {
-              var notebookBloc = BlocProvider.of<NotebooksBloc>(context);
-              notebookBloc.add(AddNotebookEvent(_controller.text));
-              Navigator.pop(context);
-            },
-            child: Text(
+            onPressed: _controller.text.isEmpty || _isExist
+                ? null
+                : () {
+                    var notebookBloc = BlocProvider.of<NotebooksBloc>(context);
+                    notebookBloc.add(AddNotebookEvent(_controller.text.trim()));
+                    Navigator.pop(context);
+                  },
+            child: const Text(
               'Create',
               style: TextStyle(fontSize: 18),
             ),
@@ -28,7 +45,7 @@ class AddNotebookScreen extends StatelessWidget {
         children: [
           TextField(
             controller: _controller,
-            style: TextStyle(
+            style: const TextStyle(
               fontSize: 20,
               fontWeight: FontWeight.bold,
             ),
@@ -37,18 +54,36 @@ class AddNotebookScreen extends StatelessWidget {
               hintStyle: TextStyle(
                 color: Colors.grey[400],
               ),
-              prefixIcon: Icon(Icons.book),
+              prefixIcon: const Icon(
+                CustomIcons.notebook_filled,
+                color: Colors.black,
+              ),
               iconColor: Colors.black,
-              border: OutlineInputBorder(borderSide: BorderSide.none),
+              border: const OutlineInputBorder(borderSide: BorderSide.none),
             ),
+            onChanged: (value) {
+              var index = notebookBloc.state.loadedNotebooks
+                  .indexWhere((nb) => nb.title == value.trim());
+              if (index >= 0) {
+                setState(() {
+                  _isExist = true;
+                  title = value;
+                });
+              } else {
+                setState(() {
+                  _isExist = false;
+                  title = '';
+                });
+              }
+            },
           ),
-          Divider(
+          const Divider(
             endIndent: 13,
             indent: 13,
             height: 2,
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 13, vertical: 10),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 13, vertical: 10),
             child: Text(
               'Notebooks are useful for grouping notes around a common topic.',
               style: TextStyle(
@@ -57,6 +92,17 @@ class AddNotebookScreen extends StatelessWidget {
               ),
             ),
           ),
+          if (_isExist)
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 13, vertical: 10),
+              child: Text(
+                'Error: There is already a notebook named "$title". Please use a unique name.',
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 15,
+                ),
+              ),
+            ),
         ],
       ),
     );
